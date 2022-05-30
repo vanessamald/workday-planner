7debdb6ab69686525ded042e77ffb26ea718dc26
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-05-29 23:15:58 -0500

    add function for past, present, future task hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ $(".saveBtn").click(function() {
     console.log(textarea);
     console.log(this);
 
+    
+
     // local storage
     localStorage.setItem(taskHour, textarea); 
 });
@@ -30,5 +32,30 @@ $("#four .text").val(localStorage.getItem("four"));
 $("#five .text").val(localStorage.getItem("five"));
 
 
+// add red and green background according to time lapse
+function checkHour() {
+    // check for current time and textarea time
+    var currentTime = moment().format('LT');
+    var sectionTime = $(".time-block").attr("id");
+    console.log(currentTime);
+    console.log(sectionTime);
+    if (currentTime > sectionTime) {
+        $(".time-block").addClass("past");
+        $(".time-block").removeClass("future");
+        $(".time-block").removeClass("present");
+    }
+    if (currentTime === sectionTime) {
+        $(".time-block").addClass("present");
+        $(".time-block").removeClass("past");
+        $(".time-block").removeClass("future");
+    }
+    if (currentTime < sectionTime) {
+        $(".time-block").addClass("future");
+        $(".time-block").removeClass("past");
+        $(".time-block").removeClass("present");
+    }
+};
+
+checkHour();
 
 
