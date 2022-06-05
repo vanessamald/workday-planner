2244d64dca4dac9c56e3f3a79489e52248b6cdeb
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-06-05 17:05:32 -0500

    fixing past, present, future classes not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,64 @@
-var text = "";
-
 // display current date and time
 $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-var tasksArray = [];
-
 // function to save tasks
 $(".saveBtn").click(function() {
+    // get time 
     var taskHour = $(this).attr("data");
+    // get task 
     var textarea = $(this).siblings("#tasks").val();
     console.log(taskHour);
     console.log(textarea);
-    console.log(this);
-
-    
+    //console.log(this);
 
     // local storage
     localStorage.setItem(taskHour, textarea); 
 });
 
 // get info from local storage for each hour 
-$("#eight .text").val(localStorage.getItem("eight"));
-$("#nine .text").val(localStorage.getItem("nine"));
-$("#ten .text").val(localStorage.getItem("ten"));
-$("#eleven .text").val(localStorage.getItem("eleven"));
-$("#twelve .text").val(localStorage.getItem("twelve"));
-$("#one .text").val(localStorage.getItem("one"));
-$("#two .text").val(localStorage.getItem("two"));
-$("#three .text").val(localStorage.getItem("three"));
-$("#four .text").val(localStorage.getItem("four"));
-$("#five .text").val(localStorage.getItem("five"));
+$("#8 .text").val(localStorage.getItem("eight"));
+$("#9 .text").val(localStorage.getItem("nine"));
+$("#10 .text").val(localStorage.getItem("ten"));
+$("#11 .text").val(localStorage.getItem("eleven"));
+$("#12 .text").val(localStorage.getItem("twelve"));
+$("#13 .text").val(localStorage.getItem("one"));
+$("#14 .text").val(localStorage.getItem("two"));
+$("#15 .text").val(localStorage.getItem("three"));
+$("#16 .text").val(localStorage.getItem("four"));
+$("#17 .text").val(localStorage.getItem("five"));
 
-
-// add red and green background according to time lapse
 function checkHour() {
-    // check for current time and textarea time
-    var currentTime = moment().format('LT');
-    var sectionTime = $(".time-block").attr("id");
-    console.log(currentTime);
-    console.log(sectionTime);
-    if (currentTime > sectionTime) {
-        $(".time-block").addClass("past");
-        $(".time-block").removeClass("future");
-        $(".time-block").removeClass("present");
-    }
-    if (currentTime === sectionTime) {
-        $(".time-block").addClass("present");
-        $(".time-block").removeClass("past");
-        $(".time-block").removeClass("future");
-    }
-    if (currentTime < sectionTime) {
-        $(".time-block").addClass("future");
-        $(".time-block").removeClass("past");
-        $(".time-block").removeClass("present");
-    }
-};
+    // check for current time 
+    var currentTime = moment().hour(); 
 
+    // loop through each time block 
+    $(".time-block").each(function () {
+
+    // check for textarea time
+    var sectionTime = parseInt($(this).attr("id")); 
+    console.log(currentTime, sectionTime);
+    //console.log(this);
+
+    // conditional statements to check against current time
+    if (currentTime > sectionTime) {
+        // add red, green, or grey background according to time lapse
+        $(this).addClass("past-tasks");
+        $(this).removeClass("future-tasks");
+        $(this).removeClass("present-tasks");  
+    }
+    else if (currentTime === sectionTime) {
+        $(this).removeClass("past-tasks"); 
+        $(this).removeClass("future-tasks");
+        $(this).addClass("present-tasks");
+    }
+    else if (currentTime < sectionTime) {
+        $(this).removeClass("present-tasks");
+        $(this).removeClass("past-tasks");
+        $(this).addClass("future-tasks");      
+        }  
+    })  
+};
+// run function 
 checkHour();
 
 
